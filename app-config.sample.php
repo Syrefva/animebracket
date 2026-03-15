59f5c24d80b6db71032b48fb153a4723a3dfa4c7
Syrefva <xsyre777@gmail.com>
2026-03-14 20:10:53 -0700

    update image url

--- app-config.sample.php
+++ app-config.sample.php
@@ -18,7 +18,7 @@ define('REDDIT_HANDLER', 'http://reddit_oauth_handler');
 define('REDDIT_MINAGE', 2592000);
 
 define('IMAGE_LOCATION', '/path/to/images');
-define('IMAGE_URL', 'http://url.to/images');
+define('IMAGE_URL', 'http://localhost:8080/images');
 
 define('REDIS_SERVER', 'tcp://127.0.0.1:6379');
 define('CACHE_PREFIX', 'AnimeBracket');
