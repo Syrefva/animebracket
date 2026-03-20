a0734e9eb413ec6578c1184467332a75d426ccff
Syre <xsyre777@gmail.com>
2026-03-20 03:25:18 -0700

    char.min.js needs to be copied too

--- webpack.config.js
+++ webpack.config.js
@@ -64,7 +64,8 @@ module.exports = (env, argv) => {
       new CopyPlugin({
         patterns: [
           { from: path.resolve(__dirname, 'static/images/'), to: 'static/images/' },
-          { from: path.resolve(__dirname, 'static/js/jquery.Jcrop.min.js'), to: 'static/js/' }
+          { from: path.resolve(__dirname, 'static/js/jquery.Jcrop.min.js'), to: 'static/js/' },
+          { from: path.resolve(__dirname, 'static/js/Chart.min.js'), to: 'static/Chart.min.js' },
         ],
       }),
     ],
